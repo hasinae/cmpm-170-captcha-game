a04a94de6ef3cd475479557e0406c13e263db940
hasina <113416278+hasinae@users.noreply.github.com>
2024-11-13 12:04:05 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
+import IntroScene from './Scenes/IntroScene.js';
 import CaptchaScene from './Scenes/CaptchaScene.js';
 
 // Game configuration
 const config = {
-    parent: 'gameContainer', // Make sure this matches the container ID in index.html
+    parent: 'gameContainer',
     type: Phaser.CANVAS,
     render: {
         pixelArt: true 
     },
     width: 1000,
     height: 800,
-    backgroundColor: '#39FF14', // Neon green background color
-    scene: [CaptchaScene]
+    backgroundColor: '#39FF14',
+    scene: [IntroScene, CaptchaScene]
 };
 
 const game = new Phaser.Game(config);
